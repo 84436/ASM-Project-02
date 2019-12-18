659cf7361e25ed6785818db42513f64f70f8691e
84436 <84436.t@gmail.com>
2019-12-18 16:46:31 +0700

    Array: tất cả trừ Search; Commons: tạm thời loại bỏ

--- Array/Array.asm
+++ Array/Array.asm
@@ -1,48 +1,45 @@
+##################################################
 # Main program
 
-.include    "Array_parts.asm"
-.include    "../syscall.asm"
+.include        "Array_parts.asm"
+                
+                .text
 
-            .data
-mstr1:      .asciiz "Enter a choice, or press Cancel/close this dialog to exit.\n1 = Valid choice\n2 = Still a valid choice"
-mvalid:     .asciiz "Valid choice."
-minvalid:   .asciiz "Invalid choice."
-mempty:		.asciiz "Please make a choice."
-
-            .text
-main:
-            li $v0, InputDialogInt
-            la $a0, mstr1
-            syscall
-            
-            beq $a1, InputDialogInt_Cancel, exit
-            beq $a1, InputDialogInt_OKonEmpty, pleasechoose
-            
-            beq $a0, 1, valid
-            beq $a0, 2, valid
-            j invalid
-            
-valid:
-            li $v0, MessageDialog
-            la $a0, mvalid
-            li $a1, MessageDialog_Info
-            syscall
-            j main
-
-invalid:
-            li $v0, MessageDialog
-            la $a0, minvalid
-            li $a1, MessageDialog_Error
-            syscall
-            j main
-
-pleasechoose:
-            li $v0, MessageDialog
-            la $a0, mempty
-            li $a1, MessageDialog_Error
-            syscall
-            j main
-
-exit:
-            li $v0, Exit
-            syscall
+input_array:    move $s0, $sp
+                sub $s0, $s0, 4
+                GetArraySize
+                GetArray
+                
+menu:           PrintChar (LF)
+                PrintChar (LF)
+                PrintString (str_menu)
+                GetInt
+                beq $v0, 0, exit
+                beq $v0, 1, f1
+                beq $v0, 2, f2
+                beq $v0, 3, f3
+                beq $v0, 4, f4
+                beq $v0, 5, f5
+                j invalid
+                
+invalid:        PrintString (str_menu_invalid)
+                j menu
+                
+f1:             List
+                j menu
+                
+f2:             Sum
+                j menu
+                
+f3:             ListPrime
+                j menu
+                
+f4:             Max
+                j menu
+                
+f5:             Search
+                j menu
+                
+exit:           PrintString (str_exit)
+                li $v0, EXIT
+                syscall
--- Array/Array_parts.asm
+++ Array/Array_parts.asm
@@ -1,42 +1,180 @@
-# Component
+##################################################
+# Syscall mappings
 
-.macro spush (%register)
-    sub $sp, $sp, 4
-    sw %register, ($sp)
+.eqv    INT_PUT         1
+.eqv    INT_GET         5
+.eqv    CHAR_PUT        11
+.eqv    STRING_PUT      4
+.eqv    STRING_GET      8
+.eqv    FILE_OPEN       13
+.eqv    FILE_CLOSE      16
+.eqv    FILE_PUT        15
+.eqv    FILE_GET        14
+.eqv    EXIT            10
+
+##################################################
+# Strings
+
+.eqv    LF      0xA
+.eqv    CR      0xD
+.eqv    SPACE   0x20
+
+                    .data
+str_getarraysize:   .asciiz "Array size = "
+str_getarray_1:     .asciiz "a["
+str_getarray_2:     .asciiz "] = "
+str_menu:           .asciiz "1 : List all elements\n2 : Get sum of all elements\n3 : List primes only\n4 : Find maximum element\n5 : Find an element\n0 : Exit\nEnter a choice [0-5]: "
+str_menu_invalid:   .asciiz "Invalid choice"
+str_f1:             .asciiz "List (all):\n"
+str_f2:             .asciiz "Sum = "
+str_f3:             .asciiz "List (prime only):\n"
+str_f4:             .asciiz "Max = "
+str_f5_input:       .asciiz "Enter a number: "
+str_f5_found:       .asciiz "Number found at index "
+str_f5_not_found:   .asciiz "Number not found"
+str_exit:           .asciiz "Goodbye."
+
+##################################################
+# Basic components
+
+.macro      spush (%r)
+            sub $sp, $sp, 4
+            sw %r, ($sp)
 .end_macro
 
-.macro spop (%register)
-    lw %register, ($sp)
-    add $sp, $sp, 4
+.macro      spop (%r)
+            lw %r, ($sp)
+            add $sp, $sp, 4
 .end_macro
 
-.macro isPrime (%int)
-    # U: $t0 (%int placeholder)
-    #    $t1 (REMainder)
-    # O: $v0 (0 or 1)
-    
-    pre:
-        spush ($t0)
-        spush ($t1)
-        li $t0, %int
+.macro      PrintInt (%r_int)
+            li $v0, INT_PUT
+            move $a0, %r_int
+            syscall
+.end_macro
 
-    main:
-        blt $t0, 2, ip_false
-        sub $t0, $t0, 2
-        ip_loop:
-            blt $t0, 0, ip_true
-            rem $t1, $t0, 2
-            beq $t1, 0, ip_false
-            sub $t0, $t0, 1
-            j ip_loop
-        ip_true:
-            li $v0, 1
-            j post
-        ip_false:
-            li $v0, 0
-            j post
+.macro      GetInt
+            li $v0, INT_GET
+            syscall
+.end_macro
 
+.macro      PrintChar (%l_char)
+            li $v0, CHAR_PUT
+            li $a0, %l_char
+            syscall
+.end_macro
+
+.macro      PrintString (%a_string)
+            li $v0, STRING_PUT
+            la $a0, %a_string
+            syscall
+.end_macro
+
+##################################################
+# Functions
+
+.macro      GetArraySize
+    func:   PrintString (str_getarraysize)
+            GetInt
+            ble $v0, 0, func
+            move $s1, $v0
+.end_macro
+
+.macro      GetArray
+            li $t0, 0
+    func:   PrintString (str_getarray_1)
+            PrintInt ($t0)
+            PrintString (str_getarray_2)
+            GetInt
+            ble $v0, 0, func
+            spush ($v0)
+            add $t0, $t0, 1
+            blt $t0, $s1, func
+.end_macro
+
+.macro      List
+            move $t0, $s0
+            li $t1, 0
+            PrintString (str_f1)
+            j func
+    sprt:   PrintChar (SPACE)
+    func:   li $v0, INT_PUT
+            lw $a0, ($t0)
+            syscall
+            sub $t0, $t0, 4
+            add $t1, $t1, 1
+            blt $t1, $s1, sprt
+.end_macro
+
+.macro      Sum
+            move $t0, $s0
+            li $t1, 0
+            li $t2, 0
+    func:   lw $a0, ($t0)
+            add $t2, $t2, $a0
+            sub $t0, $t0, 4
+            add $t1, $t1, 1
+            blt $t1, $s1, func
+    post:   PrintString (str_f2)
+            PrintInt ($t2)
+            
+.end_macro
+
+.macro      Max
+            move $t0, $s0
+            li $t1, 0
+            li $t2, 0
+    func:   lw $a0, ($t0)
+            bgt $a0, $t2, max
+            sub $t0, $t0, 4
+            add $t1, $t1, 1
+            blt $t1, $s1, func
+            j post
+    max:    move $t2, $a0
+            j func
+    post:   PrintString (str_f4)
+            PrintInt ($t2)
+.end_macro
+
+.macro      isPrime (%r_int)
+    pre:    spush ($t0)
+            spush ($t1)
+            spush ($t2)
+            move $t0, %r_int
+            li $t1, 1
+    func:   blt $t0, 2, notp
+            beq $t0, 2, p
+            sub $t2, $t0, 1
+    loop:   add $t1, $t1, 1
+            rem $v0, $t0, $t1
+            beq $v0, 0, notp
+            blt $t1, $t2, loop
+            j p
+    notp:   li $v0, 0
+            j post
+    p:      li $v0, 1
+            j post
+    post:   spop ($t2)
+            spop ($t1)
+            spop ($t0)
+.end_macro
+
+.macro      ListPrime
+    pre:    move $t0, $s0
+            li $t1, 0
+            PrintString (str_f3)
+    func:   lw $a0, ($t0)
+            isPrime ($a0)
+            beq $v0, 0, next
+            PrintInt ($a0)
+    next:   sub $t0, $t0, 4
+            add $t1, $t1, 1
+            beq $t1, $s1, post
+            beq $v0, 0, nosp
+            PrintChar (SPACE)
+    nosp:   j func
     post:
-        spop ($t1)
-        spop ($t0)
+.end_macro
+
+.macro      Search
 .end_macro
