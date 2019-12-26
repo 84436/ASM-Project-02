86cdd65a99b907a71bef9d765cd655e8cbd2a9f9
t3bol90 <becute26112000@gmail.com>
2019-12-27 03:33:05 +0700

    Update QuickSort, still some cute bugs

--- QuickSort/Quicksort.asm
+++ QuickSort/Quicksort.asm
@@ -1,11 +1,126 @@
 .include "Unity.asm"
 
-.data
-.eqv CR 0xD
-.eqv LF 0xA
-.eqv SPACE 0x20
 Title()
-open_file("/home/t3bol90/Documents/KIENTRUCMAYTINHVAHOPNGU/Project02/input_sort.txt")
-move $s0,$v0 #Move file descriptor to $s0
-loadfile($s0)
-print_int($v1)
+open_file("./input_sort.txt",0)
+move $s0,$v0 				# Move file descriptor to $s0
+loadfile($s0) 				# Load da file, $v1 store n of element, $sp store arr[n-1]
+move $s7,$v1
+
+			
+main: 						# Prepare data for Quicksort
+li $a1, 0 					# a1 = 0
+subi $a2,$v1,1 				# $a2 = n - 1
+mul $t2,$a2,4
+add $t1,$sp,$t2 			#t1 = &a[0]
+la $a0,($t1) 				#a0 = &a[0]
+
+#Reset reg
+li $t1, 0
+li $t2, 0
+li $t3, 0
+li $t4, 0
+li $t5, 0
+li $t6, 0
+li $t7, 0
+li $t8, 0
+li $t9, 0
+li $v0, 0
+
+
+jal quick_sort
+
+j write_to_file
+	
+quick_sort:					# Takes 3 args: $a0: Array, $a1: Low index, $a2: High Index			
+
+addi $sp, $sp, -12 			# Create space on the stack for 3 words
+sw $ra, 4($sp) 				# save return address
+sw $a1, 0($sp) 				# save arg 1
+sw $a2, 8($sp)				# save arg 2
+
+blt $a1, $a2, continue_sort	# Branches if the low index is bigger than the high
+
+j end_sort					# Terminate current sort process
+
+continue_sort:
+
+jal partition				# Jump to partition process. Args are the same as current quick_sort args
+
+add $t1, $v0, $zero			# Store temp variable to hold partition return value
+
+add $t2, $a2, $zero			# Store $a2 value in a temp to preserve through recursive call
+
+addi $a2, $t1, -1			# Change the high index arg to value returned by partition - 1
+
+jal quick_sort
+
+add $a2, $t2, $zero			# Restore $a2 value after recursive call	
+addi $a1, $t1, 1			# Change the High index arg to value returned by partition + 1
+
+jal quick_sort
+
+end_sort:
+lw $ra 4($sp)				# Load previous return address
+lw $a1, 0($sp)				# Load previous arg 1
+lw $a2, 8($sp)				# Load previous arg 2
+		
+addi $sp, $sp, 12			# Remove current stack frame from the stack
+jr $ra						# Jump to return address
+
+partition:					# Takes 3 args: $a0: Array, $a1: Low index, $a2: High Index
+
+addi $sp, $sp, -8 			# Create space for two words
+sw $ra, 4($sp) 				# save return address
+
+mul $t2, $a2, -4			# Calculates offset of the High Index
+add $t3, $a0, $t2 			# $t3 High address	# Calculates the address of the Pivot
+lw $t4, 0($t3)				# Loads the value thats stored in the address List[High Index] int $t4 Pivot
+
+addi $t5, $a1, -1			# Get index of the smaller element i 
+mul $t2, $t5, -4			# Calulates i offset
+add $t7, $a0, $t2 			# $t7 I address	# Calculates the address of the ith element
+
+add $t0, $a1, $zero			# Initialize loop counter j
+for_loop:
+bge $t0, $a2, exit_loop		# Loop condition: Branch if inex is greater than the high Index
+
+mul $t2, $t0, -4			# Calulates loop counter offset
+add $t6, $a0, $t2 			#$t6 J address		# Calculates the address of the jth element
+lw $t8, 0($t6)				# Loads value stored in the jth element
+
+bgt $t8, $t4, end_loop_cond			# Branch if List[j] >= Pivot
+
+addi $t5, $t5, 1			# increment i
+addi $t7, $t7, -4			# increment i
+
+lw $t9, 0($t7)				# temp = List[i]
+sw $t8, 0($t7)				# List[i] = List[j]
+sw $t9, 0($t6)				# List[j] = temp
+end_loop_cond:
+
+addi $t0, $t0, 1			# Increment i
+j for_loop
+
+exit_loop:
+
+lw $t9, -4($t7)				# temp = arr[i+1];
+sw $t4, -4($t7)				# arr[i+1] = arr[high];
+sw $t9, 0($t3)				# arr[high] = temp;
+
+addi $v0, $t5, 1			# Load return value
+
+end_of_partition:
+lw $ra, 4($sp)				# Restore previous return address
+addi $sp, $sp, 8			# Remove current stack frame from the stack
+jr $ra						# Jump to return address
+
+
+write_to_file:
+	open_file("./output_sort.txt",1)
+	move $s2,$v0
+	save_file($s2,$s7)
+	close_file($s2)
+	close_file($s0)
+	addi $v0,$0, 10
+	syscall
+
--- QuickSort/Unity.asm
+++ QuickSort/Unity.asm
@@ -1,7 +1,9 @@
-#Marcro for print string
+# Marcro for print string
 .data
 buffer: .space 10
-
+.eqv CR 0xD
+.eqv LF 0xA
+.eqv SPACE 0x20
 .eqv BASE 10
 
 .macro print_str (%str)
@@ -12,13 +14,23 @@ myLabel: .asciiz %str
 	la $a0, myLabel
 	syscall
 .end_macro
-#Marcro for print integer
+# Marcro for print integer
 .macro print_int (%x)
 	li $v0, 1
 	move $a0, %x
 	syscall
 	print_str("\n")
 .end_macro
+
+.macro printf_int(%x)
+	li $v0, 1
+	move $a0, %x
+	syscall
+	li $v0, 11
+	li $a0, SPACE
+	syscall
+.end_macro
+
 .macro for (%regIterator, %from, %to, %bodyMacroName)
 	add %regIterator, $zero, %from
 	Loop:
@@ -27,19 +39,29 @@ myLabel: .asciiz %str
 		ble %regIterator, %to, Loop
 .end_macro
 
+.macro print_list(%size)
+li $t1, 0
+loop:
+beq $t1,%size,done
+addi $t1,$t1,1
+spop($t2)
+printf_int($t2)
+j loop
+done:
+.end_macro
 
 .macro Title()
 print_str("Quicksort mini MIPS program\n")
 .end_macro 
 
 #Open file and return file descriptor to $v0
-.macro open_file (%path)
+.macro open_file (%path, %mode) # mode: 0 read , 1 write
 .data
 openfile: .asciiz %path
 .text
 li $v0, 13
 la $a0, openfile
-li $a1, 0
+li $a1, %mode
 li $a2, 0
 syscall
 move $t0,$v0
@@ -51,7 +73,8 @@ print_str("\n")
 move $v0,$t0
 .end_macro
 
-#Get char from buffer - but first, let make a move, like dacin queen
+
+#Get char from buffer - but first, let's make a move, like dancin queen
 .macro get_char(%descriptor)
 .text
 li $v0,14
@@ -66,6 +89,10 @@ lb $v0, buffer
 #Char to integer
 .macro ctoi(%c)
 subi %c,%c,48
+.end_macro
+
+.macro itoc(%i)
+addi %i,%i,48
 .end_macro
 
 #Convert base from ascii to decimal
@@ -110,7 +137,7 @@ while:
 	j while
 return_val:
 	beq $t3,0,first_push
-	print_int($t1)
+	printf_int($t1)
 	spush($t1)
 	li $t1,0
 	j while
@@ -126,5 +153,65 @@ first_push:
 outwhile:
 	move $v0,$t3
 	move $v1,$t4
-	print_str("Load file hoan tat")
+	print_str("\nLoad file complete\n")
+.end_macro
+
+.macro save_file(%descriptor,%size)
+move $a0, %descriptor
+li $t7, 0
+while1:
+	beq $t7,%size,done
+	addi $t7,$t7,1
+	spop($t8)
+	itos($t8)
+	la $a1,buffer
+	move $a2,$v1
+	addi $a2,$a2,1 #For space
+	li $v0,15
+	syscall
+	j while1
+done:
+	print_str("Save file complete\n")
+.end_macro
+
+
+
+.macro itos(%i) # Store i in buffer as string rep, return number of byte $v1
+la $t6,buffer 	# Save buffer
+beqz %i,null_buff
+li $t2, 0
+li $t5, 10
+while:
+	beqz %i,done
+	addi $t2,$t2,1
+	div %i,$t5
+	mfhi $t3
+	spush($t3)
+	mflo %i
+	j while
+null_buff:
+	itoc(%i)
+	sb %i, ($t6)
+	addi $t6,$t6,1
+	li $v1, 1
+	j end
+done:
+	move $v1,$t2
+	while2:
+		beqz $t2,end
+		subi $t2,$t2,1
+		spop($t1)
+		itoc($t1)
+		sb $t1,($t6)
+		addi $t6,$t6,1
+		j while2
+end:
+	li $t1,SPACE
+	sb $t1, ($t6)
+.end_macro
+
+.macro close_file(%descriptor)
+move $a0,%descriptor
+li $v0,16
+syscall
 .end_macro
