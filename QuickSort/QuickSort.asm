4c9bfda62e8f6f22df196e76561aa1b19ac5694b
84436 <84436.t@gmail.com>
2019-12-27 00:23:08 +0700

    GitHub bị gì vậy ta

--- QuickSort/QuickSort.asm
+++ QuickSort/QuickSort.asm
@@ -1,4 +1,11 @@
-# Main program
+.include "Unity.asm"
 
-.include    "QuickSort_parts.asm"
-.include    "../syscall.asm"
+.data
+.eqv CR 0xD
+.eqv LF 0xA
+.eqv SPACE 0x20
+Title()
+open_file("/home/t3bol90/Documents/KIENTRUCMAYTINHVAHOPNGU/Project02/input_sort.txt")
+move $s0,$v0 #Move file descriptor to $s0
+loadfile($s0)
+print_int($v1)
